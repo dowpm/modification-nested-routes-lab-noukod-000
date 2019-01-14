69bdad91d8a8369416e5be19d12f2633dbaddd1c
Mac Dowell Prosper <macdowell@noukod.com>
2019-01-14 16:52:17 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -4,7 +4,7 @@ module SongsHelper
       select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
     else
       #hidden_field_tag "song[artist_id]", song.artist_id
-      text_field_tag "song[artist_id]","song[artist_id]", disabled: true
+      text_field_tag "song[artist_id]",artist.name, disabled: true
     end
   end
 end
